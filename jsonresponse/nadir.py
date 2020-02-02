6cfc50dccb16b1a4d19e52d436222724f0750904
Achmet Tachsin <tachsinachmet@gmail.com>
2020-02-03 01:42:30 +0200

    table adjustments

--- jsonresponse/nadir.py
+++ jsonresponse/nadir.py
@@ -15,13 +15,17 @@ def getBooks():
             if div:
                 books_dict = {}
                 title_text = div.h4.a.select_one('span').text
-                title = title_text if len(title_text) < 50 else title_text[:50]
-                date = div.ul.find_all('li')[1].select_one('span').text[-4:]
+                author_text = div.p.text
+                date_text = div.ul.find_all('li')[1].select_one('span').text[-4:]
 
-                books_dict['title'] = title
-                books_dict['author'] = div.p.text
+                #title = title_text if len(title_text) < 50 else title_text[:50]
+                #author = author_text if len(author_text) < 30 else author_text[:30]
+                date = date_text if date_text.isdigit() else ""
+
+                books_dict['title'] = title_text
+                books_dict['author'] = author_text
                 books_dict['href'] = div.h4.a.get('href')
-                books_dict['published'] = date if date.isdigit() else ""
+                books_dict['published'] = date
 
                 books_list.append(books_dict)
 
